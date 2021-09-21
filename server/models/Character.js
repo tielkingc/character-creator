6681be14600dfd4a96c10c246b7dbc26ae03e7be
Christian Tielking <tielking5@gmail.com>
2021-09-21 17:59:03 -0400

    added user model

---
+++ server/models/Character.js
@@ -0,0 +1,54 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema(
+    {
+        first_name: {
+            type: String,
+            required: true,
+            trim: true 
+        },
+
+        last_name: {
+            type: String,
+            required: true,
+            trim: true
+        },
+
+        username: {
+            type: String,
+            require: true,
+            trim: true
+        },
+
+        password: {
+            type: String,
+            require: true,
+            trim: true,
+            minlength: 8
+        },
+
+        characters: {
+            type: Schema.Types.ObjectId,
+            ref: 'Character'
+        } 
+    }
+)
+
+userSchema.pre('save', async function(next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+        this.admin = this.email.includes("slwarehouse")? true:false
+    }
+  
+    next();
+});
+
+userSchema.methods.isCorrectPassword = async function(password) {
+        return await bcrypt.compare(password, this.password);
+};
+
+const User = model('User', userSchema);
+
+module.exports = User;
